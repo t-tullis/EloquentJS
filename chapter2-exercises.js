b70cc149f2d1feea51a4dc778886722f45e01d5f
t-tullis <t.tullis34@gmail.com>
2019-12-05 22:53:03 -0800

    Chapter 2 exercises

---
+++ chapter2-exercises.js
@@ -0,0 +1,33 @@
+// Exercise 1 Looping a Triangle
+
+// for(let str = '#'; str.length < 7; str += '#'){
+//     console.log(str)
+// }
+
+/*
+Exercise 2 FizzBuzz
+
+- Print all numbers from 1 - 100
+- for numbers divisible by 3 print "Fizz"
+- for numbers divisible by 5 print "Buzz"
+- for numbers divisible by 3 & 5 print "FizzBuzz "
+*/
+
+// for(let i = 1; i <= 100; i ++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log('FizzBuzz')
+//     }
+//     else if(i % 3 === 0){
+//         console.log('Fizz')
+//     }
+//     else if(i % 5 === 0){
+//         console.log('Buzz')
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+// Exercise 3 Chessboard
+
+
