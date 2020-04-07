fa3639bd5bdb34f5c73882a9f639edeeaabcbed5
t-tullis <t.tullis34@gmail.com>
2020-04-07 03:51:16 -0700

    working through eloquent JS again

--- chapter2-exercises.js
+++ chapter2-exercises.js
@@ -14,20 +14,24 @@ Exercise 2 FizzBuzz
 - for numbers divisible by 3 & 5 print "FizzBuzz "
 */
 
-// for(let i = 1; i <= 100; i ++){
-//     if(i % 3 === 0 && i % 5 === 0){
-//         console.log('FizzBuzz')
-//     }
-//     else if(i % 3 === 0){
-//         console.log('Fizz')
-//     }
-//     else if(i % 5 === 0){
-//         console.log('Buzz')
-//     }
-//     else{
-//         console.log(i)
-//     }
-// }
+function fizzBuzz(n){
+for(let i = 1; i <= n; i ++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz')
+    }
+    else if(i % 3 === 0){
+        console.log('Fizz')
+    }
+    else if(i % 5 === 0){
+        console.log('Buzz')
+    }
+    else{
+        console.log(i)
+    }
+}
+}
+
+fizzBuzz(100)
 
 // Exercise 3 Chessboard
 //Sets board size to 8
